e2f29963b351e1967d7bd7d0e3f514eb5b0cf5c6
Jeffrey Zyjeski <jeff@zyjeski.com>
2025-07-15 14:30:31 -0400

    syntax issues

--- server.cjs
+++ server.cjs
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
-const db = require('./db.js');
+const db = require('./db.cjs');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
